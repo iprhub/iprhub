80035cae9584341791cb3426ff248bfc2db00bde
chaitanyareddyk <chaitanyareddy.kothapeta@gmail.com>
2018-09-19 09:12:56 +0530

    Added FAQ and TnC Components. Changed the menu Layout of mobile view(although it ain't working as expected), improved the responsiveness of Menu in Desktop view (fixed)

---
+++ src/components/TermsAndConditions.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Grid, Header, Segment, List, Responsive } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+
+const TnC = ({ mobile }) => (
+  <Segment
+    vertical
+    style={{
+      minHeight: mobile ? 500 : 700,
+      padding: mobile ? "0em 0em" : "7em 12em"
+    }}
+  >
+    <Header textAlign="center" style={{ fontSize: mobile ? "1.6em" : "2.3em" }}>
+      Terms And Conditions
+    </Header>
+    <container text style={{ fontSize: mobile ? "1.1em" : "1.3em" }}>
+      Terms and Conditions goes here in a text container...
+    </container>
+  </Segment>
+);
+
+export default class TermsAndConditions extends Component {
+  render() {
+    return (
+      <div>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+          <TnC />
+        </Responsive>
+        <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
+          <TnC mobile />
+        </Responsive>
+      </div>
+    );
+  }
+}

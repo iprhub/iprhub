738165d7c642d6a9b034ef80996b6b6b8b556e68
Meher <mehergm@gmail.com>
2019-04-13 04:38:40 -0500

    removed old code and added refactored to nextjs and styled components

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>iprhub</div>
+      <div>About</div>
+      <div>Contact</div>
+    </div>
+  );
+};
+
+export default Header;
--- src/__tests__/About.test.js
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer";
-import About from "../components/About";
-import React from "react";
-
-test("About component renders the about us correctly", () => {
-  const rendered = renderer.create(<About />);
-  expect(rendered.toJSON()).toMatchSnapshot();
-});
